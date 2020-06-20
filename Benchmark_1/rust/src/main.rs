8666eeee85ce7f35f699cbb66fe08f4f61d59691
TiagoMSSantos <tiago_s@outlook.pt>
2020-06-20 12:14:31 +0100

    Benchmark_1: Replace Rust vec stable sort by unstable sort which is faster

--- Benchmark_1/rust/src/main.rs
+++ Benchmark_1/rust/src/main.rs
@@ -1,78 +1,73 @@
-// use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-#[derive(Debug)]
-struct Position {
-    x: i8,
-    y: i8,
-}
 
-#[allow(dead_code)]
-impl Position {
-    fn update_x(&mut self) {
-        self.x += 1;
-    }
+// #[derive(Debug)]
+// struct Position {
+//     x: i8,
+//     y: i8,
+// }
 
-    fn update_y(&mut self) {
-        self.y += 1;
-        self.update_x();
-    }
-}
+// #[allow(dead_code)]
+// impl Position {
+//     fn update_x(&mut self) {
+//         self.x += 1;
+//     }
 
-#[allow(dead_code)]
-fn hello_world() {
-    let x = 10;
-    let mut y :&i8 = &x;
-    println!("Hello, world: {}", y);
-    y = &2;
-    println!("Hello, world: {}", y);
+//     fn update_y(&mut self) {
+//         self.y += 1;
+//         self.update_x();
+//     }
+// }
 
-    let mut pos = Position {x:1, y:2};
-    pos.x = 3;
-    pos.update_x();
-    Position::update_y(&mut pos);
+// #[allow(dead_code)]
+// fn hello_world() {
+//     let x = 10;
+//     let mut y :&i8 = &x;
+//     println!("Hello, world: {}", y);
+//     y = &2;
+//     println!("Hello, world: {}", y);
 
-    println!("pos: {{ {:#?} }}", pos);
-    let pos2 = &mut pos;
-    pos2.x = 12;
-    let pos3 = &pos2;
+//     let mut pos = Position {x:1, y:2};
+//     pos.x = 3;
+//     pos.update_x();
+//     Position::update_y(&mut pos);
 
-    println!("pos: {{ {:#?} }}", pos2);
-    println!("pos: {{ {:#?} }}", pos3);
-}
+//     println!("pos: {{ {:#?} }}", pos);
+//     let pos2 = &mut pos;
+//     pos2.x = 12;
+//     let pos3 = &pos2;
+
+//     println!("pos: {{ {:#?} }}", pos2);
+//     println!("pos: {{ {:#?} }}", pos3);
+// }
 
 
 fn read_file() -> Result<String, Box<dyn std::error::Error + 'static>> {
-    let file = File::open("../input_2.txt")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
+    let file : std::fs::File = File::open("../input_2.txt")?;
+    let mut buf_reader : std::io::BufReader<File> = BufReader::new(file);
+    let mut contents : String = String::new();
     buf_reader.read_to_string(&mut contents)?;
     return Ok(contents);
 }
 
+
 fn main() {
-    //hello_world();
-    let file_text = read_file();
-    //println!("file: {:#?}", file_text);
+    let file_text : Result<String, Box<dyn std::error::Error + 'static>> = read_file();
 
-    let text = file_text.unwrap();
-    let mut lines = text.lines();
+    let text : String = file_text.unwrap();
+    let mut lines : std::str::Lines = text.lines();
 
-    let line1 = lines.next().unwrap();
-    let mut chars_line1: Vec<char> = line1.chars().collect();
-    chars_line1.sort();
-    //println!("line 1: {:#?}", line1);
-    //println!("line 1: {:#?}", chars_line1);
+    let line1 : &str = lines.next().unwrap();
+    let mut chars_line1 : Vec<char> = line1.chars().collect::<Vec<char>>();
+    chars_line1.sort_unstable();
 
-    let line2 = lines.next().unwrap();
-    let mut chars_line2: Vec<char> = line2.chars().collect();
-    chars_line2.sort();
-    //println!("line 2: {:#?}", line2);
-    //println!("line 2: {:#?}", chars_line2);
+    let line2 : &str = lines.next().unwrap();
+    let mut chars_line2 : Vec<char> = line2.chars().collect();
+    chars_line2.sort_unstable();
 
-    let matching = chars_line1.iter().zip(&chars_line2).filter(|&(a, b)| a == b).count();
+    let matching : usize = chars_line1.iter().zip(&chars_line2).filter(|&(a, b)| a == b).count();
 
     println!("RUST");
     println!("result1 = {}", matching == chars_line1.len() && matching == chars_line2.len());
--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ TEST=${1:-Benchmark_1}
 
 # Compile
 javac -cp ${TEST}/java -d ${TEST}/java/build ${TEST}/java/src/BenchMark.java;
-cargo build --manifest-path=${TEST}/rust/Cargo.toml --release;
+RUSTFLAGS="-C target-cpu=native" cargo build --manifest-path=${TEST}/rust/Cargo.toml --release;
 scalac -cp ${TEST}/scala -d ${TEST}/scala/build ${TEST}/scala/src/BenchMark.scala;
-g++ -O3 ${TEST}/cpp/BenchMark.cpp -o ${TEST}/cpp/build/BenchMark;
+g++ -Ofast -march=native ${TEST}/cpp/BenchMark.cpp -o ${TEST}/cpp/build/BenchMark;
 
 # Run
 cd ${TEST}/java; time java -cp ./build BenchMark; cd ../..; printf "\n\n";
-cd ${TEST}/rust; time cargo run --manifest-path=Cargo.toml --release; cd ../..; printf "\n\n";
+cd ${TEST}/rust; time RUSTFLAGS="-C target-cpu=native" cargo run --manifest-path=Cargo.toml --release; cd ../..; printf "\n\n";
 cd ${TEST}/scala; time scala -cp ./build BenchMark; cd ../..; printf "\n\n";
 cd ${TEST}/cpp; time ./build/BenchMark; cd ../..; printf "\n\n";
